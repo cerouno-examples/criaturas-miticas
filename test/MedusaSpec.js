6dba747989b71bd22c5e75b1f1d84a20af6dabb5
Jorge Téllez <novohispano@gmail.com>
2017-11-22 15:17:50 -0600

    adds class Person to tests

--- test/MedusaSpec.js
+++ test/MedusaSpec.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect;
 const Medusa = require('../lib/Medusa.js');
+const Person = require('../lib/Person.js');
 
 describe('Medusa', function(){
   xit('has a name', function() {
